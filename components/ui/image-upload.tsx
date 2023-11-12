bed4b0a57213176497c8f2689a2e712fe15bc1ee
SidouGmr7 <goumirisidali@hotmail.com>
2023-11-12 13:48:56 +0100

    adding my uploadPreset

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="m6g63prr">
         {({ open }) => {
           const onClick = () => {
             open();
